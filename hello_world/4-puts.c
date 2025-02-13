875d682353c5e2d10511da14d6314d755d4c02d1
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-02-13 19:00:03 +0000

    a martina no le gusta usar git

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
+/*Este es pepe*/
 
 int main(void)
 {
-	printf("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
 
 	return (0);
 
