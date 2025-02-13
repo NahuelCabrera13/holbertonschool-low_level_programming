abd271899429c6d93899382e1a92535e94a181fe
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-02-13 19:35:23 +0000

    a martina no le gusta usar git

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - use el comando printf
+ *
+ * Return: 0 el codigo esta correcto
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
