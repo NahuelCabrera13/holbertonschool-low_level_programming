c4a073fc0816b0f7831f124c106be044614c6d08
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-02-14 17:14:11 +0000

    pepe

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Imprime los números del 0 al 9 en base 10
+ *
+ * Return: Siempre 0 (Éxito)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num);
+
+	putchar(num);
+	return (0);
+}
