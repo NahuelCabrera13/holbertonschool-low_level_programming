360cc3d45d2268fc13703607140fb9dae6bb6cc0
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-02-17 15:56:45 +0000

    pepe

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,18 @@
 #include <unistd.h>
 
-/**
- * print_alphabet - Imprime el alfabeto en minúsculas seguido de una nuev line
+/* void - escribe el avecedario en minuscula
+ *
  */
+
 void print_alphabet(void)
 {
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
 
 	write(1, alphabet, 27);
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
