bb694d97110eee8a685424cfbd70c536fb551600
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-02-19 12:19:07 +0000

    pepe

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * _isalpha - reconoce los caracteres en minuscula
+ * @c: es la variable aignada
+ * Return: 1 es si todo esta bien, 0 si no
+ */
+int _isalpha(int c)
+{
+	{
+		if (c >= 'a' &&  c <= 'z')
+			return (1);
+	}
+		return (0);
+}
