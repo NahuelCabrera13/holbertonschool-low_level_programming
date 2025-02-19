17184ab4e997e32863653341ef00e54062236458
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-02-19 17:17:53 +0000

    pepe

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * jack_bauer - reconoce los caracteres en minuscula
+ * Return: 1 es si todo esta bien, 0 si no,
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
