a901ab083ad346c57e11c7081d91dffaafd28043
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-02-19 14:49:42 +0000

    pepe

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * _abs - reconoce los caracteres en minuscula
+ * @n: es la variable aignada
+ * Return: 1 es si todo esta bien, 0 si no,
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (1);
+	return (0);
+}
