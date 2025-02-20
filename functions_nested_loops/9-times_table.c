fd4357789bad3a1e0e0368b534bd16e57f648e9c
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-02-20 13:20:59 +0000

    pepe

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - reconoce los caracteres en minuscula
+ * Return: 1 es si todo esta bien, 0 si no,
+ */
+void times_table(void)
+{
+	int m, n, pepe;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (n = 0; n < 10; n++)
+		{
+			pepe = m * n;
+
+		}
+		if (pepe < 10)
+		{
+			_putchar(pepe);
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		} else
+		{
+			_putchar(pepe / 10 + 0);
+			_putchar(pepe % 10 + 0);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
