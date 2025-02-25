72407a47214c1b01451a1f263b61264f22b90aaa
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-02-25 12:14:48 +0000

    pepe

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * mul - reconoce los caracteres en minuscula
+ * @a: es la variable aignada
+ * @b: pepe
+ * Return: 1 es si todo esta bien, 0 si no
+ */
+int mul(int a, int b)
+{
+
+	a = 1
+	b = 3
+
+	_putchar( a * b);
+
+	return(0);
+}
