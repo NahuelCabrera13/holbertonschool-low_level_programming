e60c50cf8a90cf9ca8e7d2baf625ed98e206faff
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-02-25 11:54:07 +0000

    pepe

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * _islower - reconoce los caracteres en minuscula
+ * @c: es la variable aignada
+ * Return: 1 es si todo esta bien, 0 si no
+ */
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+		return (0);
+}
