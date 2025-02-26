fd2d9ede12c22c387c58c41d11144e47d0b63d85
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-02-26 14:05:01 +0000

    pepe

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/**
+ *main - pepe
+ *Return: 0 todo bien
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 100; a++)
+
+		if (a % 5 == 0 && a % 3 == 0)
+	{		printf("FizzBuzz");
+	}
+	else if (a % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	else if (a % 3 == 0)
+	{
+		printf("Fizz");
+	}
+	else
+	{
+		printf("%d", a);
+	}
+	if (a != 100)
+	{
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
