b5bc124d0cce09ae14696df39755eef736c7c689
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-02-26 16:33:30 +0000

    pepe

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 100; a++)
+	for (a = 1; a <= 100; a++)
 	{
 
 	if (a % 5 == 0 && a % 3 == 0)
@@ -26,6 +26,7 @@ int main(void)
 	{
 		printf("%d", a);
 	}
+
 	if (a != 100)
 	{
 		printf(" ");
