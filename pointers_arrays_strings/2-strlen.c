f4781995973783845be05b0c756f853cee25e72e
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-02-27 13:01:03 +0000

    pepe

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * _strlen - pepe
+ *
+ * Return: i si esta todo bien
+ *
+ * @s: pepe
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
