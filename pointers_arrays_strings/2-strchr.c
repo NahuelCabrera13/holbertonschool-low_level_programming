033fd035db1fe11119a8c611e52f33b058d98b9d
Nahuelcabrera13 <nahuelcabrera200512@gmail.com>
2025-03-04 14:57:43 +0000

    pepe

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include"main.h"
+#include"stddef.h"
+
+/**
+ * _strchr - pepe supremo mega galaxia
+ * @s: pepe
+ * @c: pepe
+ * Return: esto no tengo idea
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	if (*s == '\0')
+
+		return (s);
+	return (NULL);
+}
