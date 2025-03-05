a4c7a18ee193b27292a25241287d92e58c828147
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-03-05 15:54:20 +0000

    pepe

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - pepe
+ * @haystack: cadena
+ * @needle: sub cadena 
+ * Return: Return haystack
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *h = haystack, *n = needle;
+
+		while (*h && *n && *h == *n)
+		{
+			h++;
+			n++;
+		}
+
+		if (!*n)
+			return (haystack);
+
+		haystack++;
+	}
+
+	return (NULL);
+}
