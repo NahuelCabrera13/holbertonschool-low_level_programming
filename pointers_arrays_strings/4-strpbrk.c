cab796239db49d504fe6a2b55df0e6b2c4f564af
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-03-05 14:12:26 +0000

    pepe 2 git commit -m pepe

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk - function that search a string for any of a set of bytes
+ * @s: string
+ * @accept: string
+ * Return: pointer
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, f;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+		for (f = 0; accept[f] != '\0'; f++)
+		{
+			if (s[f] == accept[f])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+
+	return (NULL);
+}
