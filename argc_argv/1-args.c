c479c8231f93045043ab994ed0c030e5ef5f4a66
root <Nahuelcabrera13>
2025-03-07 15:32:43 +0000

    pepe

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * main - funcion pepe
+ * @argc: pepe 2
+ * @argv: pepe supreo
+ * Return: es pepe la salida
+ *
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
