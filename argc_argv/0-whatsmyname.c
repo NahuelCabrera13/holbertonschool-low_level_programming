5b481b0f8b0924ac0456f99f797151f35dfda8a0
root <Nahuelcabrera13>
2025-03-07 14:05:27 +0000

    pepe

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,15 @@
 #include"main.h"
 #include<stdio.h>
 /**
- *
+ * main - funcion pepe
+ * @argc: pepe 2
+ * @argv: pepe supreo
+ * Return: es pepe la salida
  *
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	(void) argc;
-	printf ("%s\n", argv[0]);
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
