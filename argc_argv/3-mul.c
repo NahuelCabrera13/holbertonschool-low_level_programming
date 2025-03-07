9ca5f013a1af5c5a424f42dd39b8b54186e6d9d8
root <Nahuelcabrera13>
2025-03-07 16:56:37 +0000

    pepe

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -13,15 +13,17 @@ int main(int argc, char *argv[])
 {
 	int mul, num1, num2;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	mul = num1 * num2;
-
-		printf("%d\n", mul);
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
+
+		printf("%d\n", mul);
+
 	return (0);
 }
