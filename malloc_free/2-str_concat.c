918ad8963b35f15c617e9b27c6feb1c2679d4eb4
Nahuelcabrera13 <nahuelcabrera200513@gmail.com>
2025-03-14 14:35:42 +0000

    pepe

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -4,50 +4,52 @@
 #include <string.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * str_concat - pepe
+ * @s1: pepe
+ * @s2: toti
+ * Return: pepe
  */
 char *str_concat(char *s1, char *s2)
 {
-        int i, j, k, lengthS1, lengthS2, newStrLength;
+	int	i, j, k, lengthS1, lengthS2, newStrLength;
+	char	*memory;
 
-        char *memory;
+	if (s1 == NULL && s2 == NULL)
+		return (strdup(""));
 
-        if (s1 == NULL && s2 == NULL)
-                return (strdup(""));
+	if (s1 == NULL)
+		return (strdup(s2));
 
-        if (s1 == NULL)
-                return (strdup(s2));
+	if (s2 == NULL)
+		return (strdup(s1));
 
-        if (s2 == NULL)
-                return (strdup(s1));
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-        for (i = 0; s1[i] != '\0'; i++)
-                ;
-        for (j = 0; s2[j] != '\0'; j++)
-                ;
+	lengthS1 = i;
+	lengthS2 = j;
+	newStrLength = lengthS1 + lengthS2 + 1;
 
-        lengthS1 = i;
-        lengthS2 = j;
-        newStrLength = lengthS1 + lengthS2 + 1;
+	memory = malloc(newStrLength * sizeof(char));
 
-        memory = malloc(newStrLength * sizeof(char));
+	if (memory == NULL)
+		return (NULL);
 
-        if (memory == NULL)
-                return (NULL);
+	i = 0, j = 0;
 
-        i = 0, j = 0;
+	for (k = 0; k < lengthS1; k++, i++)
+	{
+		memory[k] = s1[i];
+	}
 
-        for (k = 0; k < lengthS1; k++, i++)
-        {
-                memory[k] = s1[i];
-        }
+	for (; k < newStrLength; k++, j++)
+	{
+		memory[k] = s2[j];
+	}
 
-        for (; k < newStrLength; k++, j++)
-        {
-                memory[k] = s2[j];
-        }
+	memory[k] = '\0';
+	return (memory);
+}
 
-        memory[k] = '\0';
-        return (memory);                                                                                                                                                                                         }
